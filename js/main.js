eed73e88a031e7c6a72bad366b58d3732e1926a1
hyeonukdev <lego7407@gmail.com>
2020-06-25 17:50:41 +0900

    event-resize

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const heart = document.querySelector(".heart_btn");
 const header = document.querySelector("#header");
 const sidebox = document.querySelector(".side_box");
+const variableWidth = document.querySelectorAll(".contents_box .contents");
 
 heart.addEventListener("click", function () {
   // console.log('hit')
@@ -8,16 +9,27 @@ heart.addEventListener("click", function () {
 });
 
 function resizeFunc() {
+  //console.log("resize!");
   if (pageYOffset >= 10) {
     let calcWidth = window.innerWidth * 0.5 + 167;
-    console.log(window.innerWidth * 0.5);
+    //console.log(window.innerWidth * 0.5);
 
     sidebox.style.left = calcWidth + "px";
+  }
+
+  if (matchMedia("screen and (max-width : 800px)").matches) {
+    for (let i = 0; i < variableWidth.length; i++) {
+      variableWidth[i].style.width = window.innerWidth - 20 + "px";
+    }
+  } else {
+    for (let i = 0; i < variableWidth.length; i++) {
+      variableWidth[i].removeAttribute("style");
+    }
   }
 }
 
 function scrollFunc() {
-  console.log(pageYOffset);
+  //console.log(pageYOffset);
 
   if (pageYOffset >= 10) {
     header.classList.add("on");
@@ -33,3 +45,4 @@ function scrollFunc() {
 }
 
 window.addEventListener("scroll", scrollFunc);
+window.addEventListener("resize", resizeFunc);
