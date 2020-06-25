0ba702c4f20e2ae83190014dff457fa6e808f272
hyeonukdev <lego7407@gmail.com>
2020-06-25 17:28:57 +0900

    scroll-event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-const heart = document.querySelector('.heart_btn');
+const heart = document.querySelector(".heart_btn");
+const header = document.querySelector("#header");
+const sidebox = document.querySelector(".side_box");
 
-heart.addEventListener('click', function () {
-    // console.log('hit')
-    heart.classList.toggle('on');
-})
+heart.addEventListener("click", function () {
+  // console.log('hit')
+  heart.classList.toggle("on");
+});
+
+function resizeFunc() {
+  if (pageYOffset >= 10) {
+    let calcWidth = window.innerWidth * 0.5 + 167;
+    console.log(window.innerWidth * 0.5);
+
+    sidebox.style.left = calcWidth + "px";
+  }
+}
+
+function scrollFunc() {
+  console.log(pageYOffset);
+
+  if (pageYOffset >= 10) {
+    header.classList.add("on");
+    sidebox.classList.add("on");
+
+    resizeFunc();
+  } else {
+    header.classList.remove("on");
+    sidebox.classList.remove("on");
+
+    sidebox.removeAttribute("style");
+  }
+}
+
+window.addEventListener("scroll", scrollFunc);
