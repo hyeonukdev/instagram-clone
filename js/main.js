8d27110154d58d773b5c2fbfd4e107c7b3ee1101
hyeonukdev <lego7407@gmail.com>
2020-06-27 02:09:15 +0900

    delegation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-const heart = document.querySelector(".heart_btn");
+//const heart = document.querySelector(".heart_btn");
 const header = document.querySelector("#header");
 const sidebox = document.querySelector(".side_box");
 const variableWidth = document.querySelectorAll(".contents_box .contents");
+const delegation = document.querySelector(".contents_box");
 
-heart.addEventListener("click", function () {
-  // console.log('hit')
-  heart.classList.toggle("on");
-});
+function delegationFunc(e) {
+  let elem = e.target;
+  console.log(elem);
+
+  while (!elem.getAttribute("data-name")) {
+    elem = elem.parentNode;
+
+    if (elem.nodeName == "BODY") {
+      elem = null;
+      return;
+    }
+  }
+
+  if (elem.matches('[data-name="heartbeat"]')) {
+    console.log("하트!");
+  } else if (elem.matches('[data-name="bookmark"]')) {
+    console.log("북마크!");
+  } else if (elem.matches('[data-name="share"]')) {
+    console.log("공유!");
+  } else if (elem.matches('[data-name="more"]')) {
+    console.log("더보기!");
+  }
+  elem.classList.toggle("on");
+}
 
 function resizeFunc() {
   //console.log("resize!");
@@ -23,7 +44,9 @@ function resizeFunc() {
     }
   } else {
     for (let i = 0; i < variableWidth.length; i++) {
-      variableWidth[i].removeAttribute("style");
+      if (window.innerWidth > 600) {
+        variableWidth[i].removeAttribute("style");
+      }
     }
   }
 }
@@ -33,15 +56,28 @@ function scrollFunc() {
 
   if (pageYOffset >= 10) {
     header.classList.add("on");
-    sidebox.classList.add("on");
+
+    if (sidebox) {
+      sidebox.classList.add("on");
+    }
 
     resizeFunc();
   } else {
     header.classList.remove("on");
-    sidebox.classList.remove("on");
 
-    sidebox.removeAttribute("style");
+    if (sidebox) {
+      sidebox.classList.remove("on");
+      sidebox.removeAttribute("style");
+    }
   }
+}
+
+setTimeout(function () {
+  scrollTo(0, 0);
+}, 100);
+
+if (delegation) {
+  delegation.addEventListener("click", delegationFunc);
 }
 
 window.addEventListener("scroll", scrollFunc);
